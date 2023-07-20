8afd944b2e429ac35bebee6433dc1129511fc335
Nyallu <anyallu@gmail.com>
2023-07-20 22:35:44 +0300

    Write a script that reads stdin line by line and computes metrics:

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,36 +1,42 @@
 #!/usr/bin/python3
+
 import sys
-import signal
 
-def signal_handler(sig, frame):
-    print_stats()
-    sys.exit(0)
+def print_statistics(total_size, status_codes):
+    print(f"File size: {total_size}")
+    for code in sorted(status_codes):
+        print(f"{code}: {status_codes[code]}")
 
-signal.signal(signal.SIGINT, signal_handler)
+def parse_log_line(line):
+    try:
+        parts = line.strip().split()
+        ip_address = parts[0]
+        status_code = int(parts[-2])
+        file_size = int(parts[-1])
+        return ip_address, status_code, file_size
+    except (ValueError, IndexError):
+        return None, None, None
 
-def print_stats():
-    total_size = sum(file_sizes.values())
-    print("File size:", total_size)
-    for status_code in sorted(status_codes.keys()):
-        print(f"{status_code}: {status_codes[status_code]}")
+def main():
+    total_size = 0
+    status_codes = {}
 
-file_sizes = {}
-status_codes = {}
+    try:
+        for line_count, line in enumerate(sys.stdin, 1):
+            ip, status_code, file_size = parse_log_line(line)
+            if ip is None:
+                continue
 
-try:
-    for line_count, line in enumerate(sys.stdin, 1):
-        try:
-            _, _, _, _, _, status_code, file_size = line.strip().split()[3:]
-            status_code = int(status_code)
-            file_size = int(file_size)
-        except ValueError:
-            continue
+            total_size += file_size
+            status_codes[status_code] = status_codes.get(status_code, 0) + 1
 
-        file_sizes[line_count] = file_size
-        status_codes[status_code] = status_codes.get(status_code, 0) + 1
+            if line_count % 10 == 0:
+                print_statistics(total_size, status_codes)
 
-        if line_count % 10 == 0:
-            print_stats()
+    except KeyboardInterrupt:
+        print_statistics(total_size, status_codes)
+        sys.exit(0)
 
-except KeyboardInterrupt:
-    print_stats()
+if __name__ == "__main__":
+    main()
+
